3220297d49925982fcf1f96b4cd3e8a4bee9ff3d
Elroi Belete <elroibelete777@gmail.com>
2024-06-26 00:40:19 -0500

    BruteForce Alg working!

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 import java.math.BigInteger;
-
+//Brute force Algorithm To solve a polynomial
+//Pn(x) = 1 + x + 2x^2 3x^3
 public class Main {
     public static void main(String[] args) {
         //Data description: The polynomial coefficients and teh point to be evaluated at
@@ -10,24 +11,38 @@ public class Main {
         //First round experiments: make sure tht implementations of the three methods are correct.
 
 
-//        BigInteger something = (BigInteger)obj.pnX(4, 3);
-//        System.out.print(something);
 
 
-         BigInteger val = BigInteger.valueOf(1 + 4 + 2*(int)Math.pow(123, 2) + 3*(int)Math.pow(123, 3) +
-                4*(int)Math.pow(123,4) + 5L *(int)Math.pow(123,5) + 6L *(int)Math.pow(123,6) +
-                 7L *(int)Math.pow(123,7) + 8L *(int)Math.pow(123,8) + 9L *(int)Math.pow(123,9) +
-                 10L *(int)Math.pow(123,10) + 11L *(int)Math.pow(123,11) + 12L *(int)Math.pow(123,12) +
-                 13L *(int)Math.pow(123,13) + 14L *(int)Math.pow(123,14) + 15L *(int)Math.pow(123,15) +
-                 16L *(int)Math.pow(123,16) + 17L *(int)Math.pow(123, 17) + 18L *(int)Math.pow(123,18) +
-                 19L *(int)Math.pow(123,19) + 20L *(int)Math.pow(123,20) + 21L *(int)Math.pow(123, 21) +
-                 22L *(int)Math.pow(123,22) + 23L *(int)Math.pow(123,23) + 24L *(int)Math.pow(123,24) +
-                 25L *(int)Math.pow(123,25));
-        System.out.println(val);
+        //repeated squaring technique
+
+        // graphic design x marketing
+
+        BigInteger answer = new BigInteger("0");
+        answer = formula(6000, BigInteger.valueOf(690));
+        System.out.println(answer);
 
     }
 
+    static  BigInteger poww(BigInteger num, int power){
+        BigInteger temp = new BigInteger("0");
+        temp = num;
+        for(int i = 0; i < power-1; i++){
+            num = num.multiply(temp);
+        }
+        return num;
+    }
+
+    static BigInteger formula(int n, BigInteger x){
+        BigInteger answer = new BigInteger("0");
+        for(int i=1; i < n+1; i++){
+            answer = answer.add(poww(x, i).multiply(BigInteger.valueOf(i)));
+        }
+        answer = answer.add(BigInteger.valueOf(1));
+
+        return answer;
+    }
 
 
-
+// we can set d all the coefficients are randomly generated
+    //one file oding brute force and one file doing another one
 }

37b2bf0c057539d0f4b610e0a7e398ad5cf90ff6
Elroi Belete <elroibelete777@gmail.com>
2024-07-16 22:53:54 -0500

    All is well

--- RealMain.java
+++ RealMain.java
@@ -1,30 +1,128 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.math.BigInteger;
+import java.util.Random;
+import java.util.Scanner;
 
 public class RealMain {
     public static void main(String[] args) {
-        Mainp2 bruteObj = new Mainp2();
-        PolynomialP2 repeatedSquareObj = new PolynomialP2();
-        String filePath = "answerData.txt";
+        int n = 3;
+        int d = 2;
+        String answerFile = "answerData.txt";
+        String generatedFile = "generated.txt";
+        RealMain randomizerObj = new RealMain();
+        int x = randomizerObj.generateRandomWithDigits(d);
+        int[] coeffs = randomizerObj.generateCoeffsWithRandDigits(n, d);
 
-        BigInteger answer = new BigInteger("0");
+        BigInteger bruteAnswer = new BigInteger("0");
+        BigInteger repeatAnswer = new BigInteger("0");
+        BigInteger hornerAnswer = new BigInteger("0");
+
+        long bruteTime = 0;
+        long repeatTime = 0;
+        long hornerTime = 0;
+
+        PolynomialP3 repeatObj = new PolynomialP3();
+        MainP3 bruteObj = new MainP3();
+        HornersRuleP3 hornerObj = new HornersRuleP3();
+
+
+        try(PrintWriter writer = new PrintWriter(generatedFile)){
+            writer.println(x);
+            writer.println(n);
+            for(int i = 0; i < coeffs.length; i++){
+                writer.println(coeffs[i]);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        File readFile = new File(generatedFile);
+        try {
+            Scanner scanObj = new Scanner(readFile);
+            int isX = scanObj.nextInt();
+            int isN = scanObj.nextInt();
+            int[] coefficients = new int[n+1];
+            int j = 0;
+            for(int i = 0; i < coefficients.length; i++){
+                coefficients[i] = scanObj.nextInt();
+            }
+            scanObj.close();
+            System.out.println("HEEEEEYYYYYYYYYY\n\n");
+            for(int i =0; i < coefficients.length; i++){
+                System.out.println(coefficients[i]);
+            }
+            scanObj.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+
+
         long startTime = System.currentTimeMillis();
-        answer = bruteObj.formula(10000, BigInteger.valueOf(3));
+        bruteAnswer = bruteObj.formula(BigInteger.valueOf(x), coeffs);
         long endTime = System.currentTimeMillis();
-        try(PrintWriter writer = new PrintWriter(filePath)){
-            writer.println("Answer: " + answer);
-            writer.println("It took " + (endTime - startTime));
+        bruteTime = endTime - startTime;
+
+        startTime = System.currentTimeMillis();
+        repeatAnswer = repeatObj.formula(x, coeffs);
+        endTime = System.currentTimeMillis();
+        repeatTime = endTime - startTime;
+
+        startTime = System.currentTimeMillis();
+        hornerAnswer = hornerObj.horner(x, coeffs);
+        endTime = System.currentTimeMillis();
+        hornerTime = endTime - startTime;
+
+
+
+        try(PrintWriter writer = new PrintWriter(answerFile)){
+            writer.println("Answer for bruteForce: " + bruteAnswer);
+            writer.println("It took " + bruteTime + " seconds \n");
+            writer.println("Answer for repeatSquaringTech " + repeatAnswer);
+            writer.println("It took " + repeatTime + " seconds \n");
+            writer.println("Answer for Horner's rule " + hornerAnswer);
+            writer.println("It took " + hornerTime + " seconds");
+
         } catch (IOException e) {
             e.printStackTrace();
         }
 
 
 
-        startTime = System.currentTimeMillis();
-        System.out.println(repeatedSquareObj.formula(10000, 3));
-        endTime = System.currentTimeMillis();
-        System.out.println(endTime - startTime);
+
 
     }
+
+
+
+
+
+
+    public int[] generateCoeffsWithRandDigits(int n, int d){
+        int[] coeffs = new int[n+1];
+        for(int i = 0; i < coeffs.length; i++){ // was n+1
+            coeffs[i] = generateRandomWithDigits(d);
+        }
+        System.out.println("\n");
+        for (int coeff : coeffs) {
+            System.out.println(coeff);
+        }
+        return coeffs;
+    }
+    public int generateRandomWithDigits(int d) {
+        Random rand = new Random();
+        int minimum = (int) Math.pow(10, d-1);
+        int maximum = (int) Math.pow(10, d) - 1;
+        return rand.nextInt(maximum - minimum + 1) + minimum;
+    }
+
+
 }
